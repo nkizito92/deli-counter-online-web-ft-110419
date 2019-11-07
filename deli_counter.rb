90b1b306e15eda978093efed4b1967472f0a3b01
Kizito Njoku <nkizito57@gmail.com>
2019-11-07 01:34:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 
 def now_serving(person)
   if person.length >= 1
-    puts "Currently serving #{person[1]}."
+    puts "Currently serving #{person[2]}."
     person.first
   else 
     puts "There is nobody waiting to be served!"
