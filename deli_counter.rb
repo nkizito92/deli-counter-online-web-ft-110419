4ff8ae273929d53fc0151aca29a4cf66aa11ff20
Kizito Njoku <nkizito57@gmail.com>
2019-11-07 01:24:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,9 +10,8 @@ end
 
 def take_a_number(array, new_ppl) 
   array.push(new_ppl)
-  array.each.with_index(1) do |person, num|
-    array.push(number)
-  puts "Welcome, #{person}. You are number #{num} in line"
+  puts "Welcome, #{new_ppl}. You are number #{} in line"
+  array.index(new_ppl)
   end 
 end
 # take_a_number(katz_deli, "Jack")
