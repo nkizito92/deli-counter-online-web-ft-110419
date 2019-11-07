44e4d857cecbb49e524744fe30fec73161fde13c
Kizito Njoku <nkizito57@gmail.com>
2019-11-07 01:46:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,19 @@
 # Write your code here
-katz_deli = ["Tom","James", "Chris"]
+#katz_deli = ["Tom","James", "Chris"]
 def line(someone) 
   cur_line = "The line is currently:" 
   someone.each.with_index(1) do |num, name|
     cur_line << " #{name}. #{num}"
   end
-  puts cur_line
+  someone.length < 1 ? (puts "The line is currently empty.") : (puts cur_line)
 end
 
 def take_a_number(array, new_ppl) 
   array.push(new_ppl)
-  array.each.with_index(1) do |number|
-    array.push(number)
-  end 
-  puts "Welcome, #{new_ppl}. You are number #{array} in line"
+  puts "Welcome, #{new_ppl}. You are number #{array.index(new_ppl)+1} in line."
 end
-take_a_number(katz_deli, "Jack")
+# take_a_number(katz_deli, "Jack")
 
 def now_serving(person)
-  if person.length >= 1
-    puts "Currently serving #{person[0]}."
-  else 
-    puts "There is nobody waiting to be served!"
-  end
+  person.length >= 1 ? (puts "Currently serving #{person.shift}.") : (puts "There is nobody waiting to be served!")
 end 
-now_serving(katz_deli)
