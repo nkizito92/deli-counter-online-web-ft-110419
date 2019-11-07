22f31c69ab6d26bef1ac3a8a160b8147f442dda5
Kizito Njoku <nkizito57@gmail.com>
2019-11-07 00:57:37 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+# Write your code here
+katz_deli = ["Tom","James", "Chris"]
+def line(someone) 
+  cur_line = "The line is currently:" 
+  someone.each.with_index(1) do |num, name|
+    cur_line << " #{name}. #{num}"
+  end
+  puts cur_line
+end
+
+def take_a_number(array, new_ppl) 
+  array.push(new_ppl)
+  array.each.with_index(1) do |number|
+    array.push(number)
+  end 
+  puts "Welcome, #{new_ppl}. You are number #{array} in line"
+end
+take_a_number(katz_deli, "Jack")
+
+def now_serving(person)
+  if person.length >= 1
+    puts "Currently serving #{person[0]}."
+  else 
+    puts "There is nobody waiting to be served!"
+  end
+end 
+now_serving(katz_deli)
